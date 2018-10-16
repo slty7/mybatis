f9408e1144c94e3e432f7495e6bf85873bdcf8e3
Administrator <Administrator@1-PC>
2018-10-17 00:18:30 +0800

    1.mybatis使用获得的数据,以及后面尽量不是用float

--- src/com/stly7/TestMybatis.java
+++ src/com/stly7/TestMybatis.java
@@ -10,20 +10,24 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.stly7.pojo.Category;
+import com.stly7.pojo.Product;
 
 public class TestMybatis {
 	public static void main(String[] args) throws IOException {
 		String resource = "mybatis-config.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory factory 
-		= new SqlSessionFactoryBuilder().build(inputStream);
-		
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+
 		SqlSession session = factory.openSession();
-		
-		List<Category> list = session.selectList("listCategory");
-		//session.selectList("com.stly7.pojo.listCategory");
-		for (Category category : list) {
+
+		List<Category> cs = session.selectList("listCategory");
+		List<Product> ps = session.selectList("com.stly7.pojo.listProduct");
+		for (Category category : cs) {
 			System.out.println(category.getId() + " : " + category.getName());
+		}
+
+		for (Product p : ps) {
+			System.out.println(p.getId() + " : " + p.getName() + " : " + p.getPrice());
 		}
 	}
 }
